926156c5c7fa5988f4eb856b9ddf6c52159eb306
felipebastosb <felipe.bastos@ymail.com>
2016-03-04 00:29:56 -0300

    Update and rename reconstruct.m to reconstrucao.m

---
+++ reconstrucao.m
@@ -0,0 +1,35 @@
+
+funcao X = reconstrucao(W, Y, mu)
+    %%  Reonstruct Y from X using W, pass mu to adjust mean.
+    %%
+    %%  Argumentos:
+    %%    C [Número de componentes x Número de informações] projeção
+    %%    D [Dimenção x Número de componentes] matriz transformada
+    %%    mu [Dimenção x 1] média simples (opicional) (Somatoria de até N dividido por N)
+    %%
+    %%  Retornos:
+    %%    X [Dimenção x Núemro de informações] Informação reconstruida
+    %%
+    
+    switch nargin     %% Verifica Quantos argumentos foram passados para a função
+    case 2            %% Caso apenas dois argumentos tenham sido passados (A projeção e a matriz transformada
+                      %% Multiplica a projeção com a matriz para se recuperar a informação (face)
+         X = W * Y;   
+    case 3            %% Caso três argumentos argumentos tenham entrado, a projeção, a matriz e mais a média simples
+                      %% Recupera a informação como no caso anterior e soma as informações que mais se repetem (mais marcantes)
+        X = W*Y +repmat(mu,size(Y,2),1);
+    otherwise        %% Caso nem um argumento tenha sido passado retorna se 0.
+        x = 0;
+    endswitch
+
+    
+    
+    
+    
+ %% Código com "if" do ByteFish   
+ %%   if(nargin<3)    %%Verifica se foram passados menos de três argumentos para a função
+ %%       X = W * Y;  %% Multiplica a projeção com a matriz para se recuperar a informação (face)
+ %%   else
+ %%       X = W*Y +repmat(mu,size(Y,2),1); %% Recupera a informação como no caso anterior e soma as informações que mais se repetem (mais marcantes)
+ %%   end
+ %% end
