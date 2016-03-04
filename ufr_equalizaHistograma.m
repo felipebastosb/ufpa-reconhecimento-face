cd0ec00f81a4d1ca66c898d2114465e997ec4f41
felipebastosb <felipe.bastos@ymail.com>
2016-03-04 09:07:17 -0300

    Update and rename spatialhistogram.m to ufr_equalizaHistograma.m

--- ufr_equalizaHistograma.m
+++ ufr_equalizaHistograma.m
@@ -1,7 +1,7 @@
 % Copyright (c) Philipp Wagner. All rights reserved.
 % Licensed under the BSD license. See LICENSE file in the project root for full license information.
 
-function SH = spatialhistogram(L, m, n, numBins)
+function EH = equalizaHistograma(L, m, n, nBins)
 	%% Calcula o histograma espacial de uma imagem LBP dividida em  [m x n] celulas.
 	%% TODO Uniformização de padroes
 	%% TODO Vetorizar
@@ -15,11 +15,11 @@ function SH = spatialhistogram(L, m, n, numBins)
 	SH = zeros(1,m*n*numBins);
 	i = 0;
   %% Para linha 1 ate linha m
-	for rowIdx=1:m
+	for linhaIdx=1:m
   %% Para coluna 1 ate coluna n
 		for colIdx=1:n
-			C = L((rowIdx-1)*py+1:rowIdx*py,(colIdx-1)*px+1:colIdx*px);
-			SH(1,(i*numBins+1):((i+1)*numBins)) = hist(C(:), numBins, 1); % histograma normalizado
+			C = L((linhaIdx-1)*py+1:linhaIdx*py,(colIdx-1)*px+1:colIdx*px);
+			EH(1,(i*nBins+1):((i+1)*nBins)) = hist(C(:), nBins, 1); % histograma normalizado
 			i+=1;
 		end
 	end
